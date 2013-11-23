6cc89e65b5df349ff00a550afaa8f874a3f414c5
Stephen Cousins <stephencousins@stephens-air.(none)>
2013-11-23 15:44:00 -0500

    Hooked up the form to the database commit

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,9 +1,25 @@
 class PicsController < ApplicationController
 
+	def new
+
+		@pic = Pic.new
+
+	end
+
+	def create
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path 
+	end
+
+
 	def index
-
 		@pics = Pic.all
+	end
 
+	private 
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
 
 	end
 
