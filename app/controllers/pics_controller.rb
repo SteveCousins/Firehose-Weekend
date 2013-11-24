380cf36367c55e8004dc76707727cadb9e402569
Stephen Cousins <stephencousins@Stephens-MacBook-Air.local>
2013-11-24 12:19:50 -0500

    Everything Done

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,14 +1,42 @@
 class PicsController < ApplicationController
 
+	def update
+		@pic = Pic.find(params[:id])
+		@pic.update_attributes( pic_params )
+		
+		if @pic.valid?
+			redirect_to pics_path 
+		else
+			render :edit, :status => :unprocessable_entity
+		end
+	end
+
+	def edit
+		@pic = Pic.find(params[:id])
+	end
+
+	def destroy
+		Pic.destroy(params[:id])
+		redirect_to pics_path
+	end
+
+
+	def show
+		@pic = Pic.find(params[:id])
+	end
+
 	def new
-
 		@pic = Pic.new
-
 	end
 
 	def create
 		@pic = Pic.create( pic_params )
-		redirect_to pics_path 
+		
+		if @pic.valid?
+			redirect_to pics_path 
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 
